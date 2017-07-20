c318408a167b7ae1b89aa0ac7897e10dc50e999f
vagueblake <tantingyu34@gmail.com>
2017-07-20 11:02:41 -0400

    3.3 to 3.10

--- audit.sh
+++ audit.sh
@@ -190,5 +190,191 @@ then
 else 
 	echo "2. X Window System - FAILED (Xorg-x11-server-common is installed and is the default graphical interface)"
 fi
+count=3
+	checkavahi=`systemctl status avahi-daemon | grep inactive`
+	checkavahi1=`systemctl status avahi-daemon | grep disabled`
+	if [ -n "$checkavahi" -a -n "$checkavahi1" ]
+	then 
+		echo "$count. Avahi-daemon - PASSED (Avahi-daemon is inactive and disabled) "
+		((count++))
+	elif [ -n "$checkavahi" -a -z "$checkavahi1" ]
+	then 
+		echo "$count. Avahi-daemon - FAILED (Avahi-daemon is inactive but not disabled)"
+		((count++))
+	elif [ -z "$checkavahi" -a -n "$checkavahi1" ]
+	then 
+		echo "$count. Avahi-daemon - FAILED (Avahi-daemon is disabled but active)"
+		((count++))
+	else 
+		echo "$count. Avahi-daemon - FAILED (Avahi-daemon is active and enabled)"
+		((count++))
+	fi
+	
+	checkcups=`systemctl status cups | grep inactive`
+	checkcups1=`systemctl status cups | grep disabled`
+	
+if [ -n "$checkcups" -a -n "$checkcups1" ]
+	then 
+		echo "$count. Cups - PASSED (Cups is inactive and disabled) "
+		((count++))
+	elif [ -n "$checkcups" -a -z "$checkcups1" ]
+	then 
+		echo "$count. Cups - FAILED (Cups is inactive but not disabled)"
+		((count++))
+	elif [ -z "$checkcups" -a -n "$checkcups1" ]
+	then 
+		echo "$count. Cups - FAILED (Cups is disabled but active)"
+		((count++))
+	else 
+		echo "$count. Cups - FAILED (Cups is active and enabled)"
+		((count++))
+	fi
+
+
+checkyumdhcp=`yum list dhcp | grep "Available package" `
+checkyumdhcpactive=`systemctl status dhcp | grep inactive `
+checkyumdhcpenable=`systemctl status dhcp | grep disabled `
+if [ -z "$checkyumdhcp" ]
+then 
+	echo "$count. DHCP Server - PASSED (DHCP is not installed) "
+	((count++))
+else 
+	if [ -z "$checkyumdhcpactive" -a -z "$checkyumdhcpenable" ]
+	then 
+		echo "$count. DHCP - FAILED (DHCP is active and enabled)"
+		((count++))
+	elif [ -z "$checkyumdhcpactive" -a -n "$checkyumdhcpenable" ]
+	then 
+		echo "$count. DHCP - FAILED (DHCP is active but disabled)"
+		((count++))
+	elif [ -n "$checkyumdhcpactive" -a -z "$checkyumdhcpenable" ]
+	then
+		echo "$count. DHCP - FAILED (DHCP is inactive but enabled)"
+		((count++))
+	else 
+		echo "$count. DHCP - FAILED (DHCP is inactive but disabled)"
+		((count++))
+	fi
+fi
+
+checkntp1=`grep "^restrict default kod nomodify notrap nopeer noquery" /etc/ntp.conf`
+checkntp2=`grep "^restrict -6 default kod nomodify notrap nopeer noquery" /etc/ntp.conf` 
+checkntp3=`grep "^server" /etc/ntp.conf | grep server`
+checkntp4=`grep 'OPTIONS="-u ntp:ntp -p /var/run/ntpd.pid"' /etc/sysconfig/ntpd `
+
+if [ -n "$checkntp1" ]
+then 
+	if [ -n "$checkntp2" ]
+	then 
+		if [ -n "$checkntp3" ]
+			then 
+				if [ -n "$checkntp4" ]
+				then
+					echo "$count. NTP - PASSED (NTP has been properly configured)"
+					((count++))
+				else 
+					echo "$count. NTP - FAILED (Option has not been configured in /etc/sysconfig/ntpd)" 
+					((count++))
+				fi
+		else
+			echo "$count. NTP - FAILED (Failed to list down NTP servers)"
+			((count++))
+		fi
+	else 
+		echo "$count. NTP - FAILED (Failed to implement restrict -6 default kod nomodify notrap nopeer noquery)"
+		((count++))
+	fi
+else 
+	echo "$count. NTP - FAILED (Failed to implement restrict default kod nomodify notrap nopeer noquery)"
+	((count++))
+fi 
+
+checkldapclients=`yum list openldap-clients | grep 'Available Packages'`
+checkldapservers=`yum list openldap-servers | grep 'Available Packages'`
+
+if [ -n "checkldapclients" -a -n "checkldapservers" ]
+then 
+	echo "$count. LDAP - PASSED (LDAP server and client are both not installed)"
+	((count++))
+elif [ -n "checkldapclients" -a -z "checkldapservers" ]
+then
+	echo "$count. LDAP - FAILED (LDAP server is installed)"
+	((count++))
+elif [ -z "checkldapclients" -a -n "checkldapservers" ]
+then
+	echo "$count. LDAP - FAILED (LDAP client is installed)"
+	((count++))
+else 
+	echo "$count. LDAP - FAILED (Both LDAP client and server are installed)"
+	((count++))
+fi 
+
+nfsservices=( "nfs-lock" "nfs-secure" "rpcbind" "nfs-idmap" "nfs-secure-server" )
+
+for eachnfsservice in ${nfsservices[*]}
+do 
+	checknfsservices=`systemctl is-enabled $eachnfsservice | grep enabled`
+	if [ -z "$checknfsservices" ]
+	then 
+		echo "$count. $eachnfsservice - PASSED ($eachnfsservice is disabled) "
+		((count++))
+	else 
+		echo "$count. $eachnfsservice - FAILED ($eachnfsservice is enabled)"
+		((count++))
+	fi
+done 	
+
+standardservices=( "named" "vsftpd" "httpd" "sshd" "snmpd") 
+
+
+for eachstandardservice in ${standardservices[*]}
+do 
+	checkserviceexist=`systemctl status $eachstandardservice | grep not-found`
+	if [ -n "$checkserviceexist" ]
+	then
+		echo "$count. $eachstandardservice - PASSED ($eachstandardservice does not exist in the system)"
+		((count++))
+	else
+		checkstandardservices=`systemctl status $eachstandardservice | grep disabled`
+		checkstandardservices1=`systemctl status $eachstandardservice | grep inactive`
+		if [ -z "$checkstandardservices" -a -z "$checkstandardservices1" ]
+		then 
+			echo "$count. $eachstandardservice - FAILED ($eachstandardservice is active and enabled) "
+			((count++))
+		elif [ -z "$checkstandardservices" -a -n "$checkstandardservices1" ]
+		then 
+			echo "$count. $eachstandardservice - FAILED ($eachstandardservice is inactive but enabled) "
+			((count++))
+		elif [ -n "$checkstandardservices" -a -z "$checkstandardservices1" ]
+		then 
+			echo "$count. $eachstandardservice - FAILED ($eachstandardservice is disabled but active) "
+			((count++))
+		else 
+			echo "$count. $eachstandardservice - PASSED ($eachstandardservice is disabled and inactive)"
+			((count++))
+		fi
+	fi
+done 	
+
+checkmailtransferagent=`netstat -an | grep ":25[[:space:]]"`
+
+if [ -n "$checkmailtransferagent" ]
+then
+	checklistening=`netstat -an | grep LISTEN`
+	if [ -n "$checklistening" ]
+	then
+		checklocaladdress=`netstat -an | grep [[:space:]]127.0.0.1:25[[:space:]] | grep LISTEN`
+		if [ -n "$checklocaladdress" ]
+		then
+			echo "$count. MTA - PASSED (Mail Transfer Agent is listening on the loopback address)"
+		else
+			echo "$count. MTA - FAILED (Mail Transfer Agent is not listening on the loopback address)"
+		fi
+	else
+		echo "$count. MTA - FAILED (Mail Transfer Agent is not in listening mode)"
+	fi
+else
+	echo "$count. MTA - FAILED (Mail Transfer Agent is not configured/installed)"
+fi
 
 
